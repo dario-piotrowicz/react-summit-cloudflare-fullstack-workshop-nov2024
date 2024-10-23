773eb868c69adf084e0b69d9a5bacb08797e256f
Dario Piotrowicz <dario@cloudflare.com>
2024-10-23 22:31:54 +0100

    exr01 done - exr02 start
    This commit includes the complete solution of exercise 01
    (including the optional steps)
    
    and it is the starting point for exercise 02

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -3,7 +3,13 @@ import type {
   LoaderFunctionArgs,
   MetaFunction,
 } from "@remix-run/cloudflare";
-import { Form, redirect, useLoaderData } from "@remix-run/react";
+import {
+  Form,
+  redirect,
+  useActionData,
+  useLoaderData,
+  useNavigation,
+} from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -33,6 +39,28 @@ export async function action({ request }: ActionFunctionArgs) {
   const title = body.get("card-title");
   const description = body.get("card-description");
 
+  const errors: { title?: string; description?: string } = {};
+
+  if (!title) {
+    errors.title = "no title was provided";
+  }
+
+  if (title instanceof File) {
+    errors.title = "title cannot be a file";
+  }
+
+  if (!description) {
+    errors.description = "no description was provided";
+  }
+
+  if (description instanceof File) {
+    errors.description = "description cannot be a file";
+  }
+
+  if (Object.keys(errors).length > 0) {
+    return { errors };
+  }
+
   console.log({
     title,
     description,
@@ -48,11 +76,20 @@ export async function action({ request }: ActionFunctionArgs) {
 
 export default function Index() {
   const cardDetails = useLoaderData<typeof loader>();
+  const actionData = useActionData<typeof action>();
+
+  const { state } = useNavigation();
+  const submitting = state === "submitting";
 
   return (
     <main className="main">
       {cardDetails === null ? (
         <Form className="card-form" method="post">
+          {submitting && (
+            <div className="card-form__loading">
+              <div className="loader"></div>
+            </div>
+          )}
           <div className="card">
             <div className="card__image"></div>
             <input
@@ -60,18 +97,32 @@ export default function Index() {
               type="text"
               name="card-title"
               id="card-title"
+              disabled={submitting}
               placeholder="title"
               required
             />
+            {actionData?.errors?.title ? (
+              <span className="card-form__error">
+                {actionData?.errors.title}
+              </span>
+            ) : null}
             <textarea
               className="card__description card__description--input"
               name="card-description"
               id="card-description"
+              disabled={submitting}
               placeholder="description..."
               required
             ></textarea>
+            {actionData?.errors?.description ? (
+              <span className="card-form__error">
+                {actionData?.errors.description}
+              </span>
+            ) : null}
           </div>
-          <button className="btn btn--generate">Generate</button>
+          <button disabled={submitting} className="btn btn--generate">
+            Generate
+          </button>
         </Form>
       ) : (
         <div className="card">
