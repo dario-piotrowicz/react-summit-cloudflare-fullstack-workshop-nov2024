3435d60068ffe840a7cdab891aa67fa17cda2365
Dario Piotrowicz <dario@cloudflare.com>
2024-10-23 22:54:03 +0100

    exr04 done - exr05 start
    This commit includes the complete solution of exercise 04
    
    Including the step 3 solution:
     implementing the image route
    
    and it is the starting point for exercise 05

--- app/routes/image.$cardId.tsx
+++ app/routes/image.$cardId.tsx
@@ -1,5 +1,21 @@
-export async function loader() {
-  throw new Response("Not Implemented", {
-    status: 500,
+import { type LoaderFunctionArgs } from "@remix-run/cloudflare";
+import { CardManager } from "~/card-manager";
+
+export async function loader({ context, params }: LoaderFunctionArgs) {
+  const cardManager = new CardManager(context.cloudflare.env);
+
+  const cardImage = await cardManager.getCardImage(params.cardId!);
+
+  if (!cardImage) {
+    throw new Response(null, {
+      status: 404,
+      statusText: "Not Found",
+    });
+  }
+
+  return new Response(cardImage, {
+    headers: {
+      "content-type": "image/png",
+    },
   });
 }
